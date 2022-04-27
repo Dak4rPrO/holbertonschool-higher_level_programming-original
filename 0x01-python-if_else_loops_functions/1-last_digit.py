b96ce3c14cebd672eae3a179c634198f01c68580
Dak4rPrO <shinoflow111@gmail.com>
2022-04-27 15:55:16 -0700

    1-last_digit.py condicion para numeros negativos

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 Ld = int(repr(number)[-1])
+if number < 0:
+    Ld = number % -10
 if Ld > 5:
     print(f"Last digit of %d is {Ld} and is greater than 5" % number)
 elif Ld == 0:
