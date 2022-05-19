c4dd09558456de4197857d3a745b5d50e39e9f97
Dak4rPrO <shinoflow111@gmail.com>
2022-05-18 20:42:00 -0700

    7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -3,10 +3,11 @@
 
 
 class Rectangle:
-    number_of_instances = 0
-    print_symbol = '#'
 
     """class Rectangle that defines a rectangle by:(based on 0-rectangle)"""
+
+    number_of_instances = 0
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         self.width = width
