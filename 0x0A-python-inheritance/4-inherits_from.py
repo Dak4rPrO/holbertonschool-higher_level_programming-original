f1c0457ab92aa6a3aa9d6953a8281d25f1b48f56
Dak4rPrO <shinoflow111@gmail.com>
2022-05-30 14:48:54 -0700

    si

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""sinosino"""
+
+
 def inherits_from(obj, a_class):
+    """unodostres"""
+
     return isinstance(obj, a_class) and a_class is not type(obj)
--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+""" class baseGeometry"""
+
+
 class BaseGeometry:
+    """ my class """
+
     pass
