b1ae9f26f797de4d0734d16ecf2827c0567df6ec
Dak4rPrO <shinoflow111@gmail.com>
2022-05-30 16:03:50 -0700

    s

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+""" class MyInt """
+
+
 class MyInt(int):
+    """ class myint """
     def __eq__(self, result):
+        """def eq"""
+
         return False
 
     def __ne__(self, result):
+        """ def ne """
         return True
