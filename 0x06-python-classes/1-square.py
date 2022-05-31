36d94c9d3a8208bc58793f1dc22d99cf071d634c
Dak4rPrO <shinoflow111@gmail.com>
2022-05-30 19:42:00 -0700

    task 1 && 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""estupido estandar"""
+
+
+class Square:
+    """estupido estandar"""
+
+    def __init__(self, size):
+        """ def init """
+
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""estupido estandar"""
+
+
+class Square:
+    """estupido estandar"""
+
+    def __init__(self, size=0):
+        """ def init """
+
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >=0")
