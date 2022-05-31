c0522aeb2b00f57c63a6cdf4ed09223bee45454c
Dak4rPrO <shinoflow111@gmail.com>
2022-05-31 15:07:20 -0700

    7

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" class Student that defines a student by """
+
+
+class Student:
+    """ class student """
+
+    def __init__(self, first_name, last_name, age):
+        """ def init """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ def attrs """
+
+        if attrs is not None:
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ def json """
+
+        
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,9 +9,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 try:
-    file = []
-except Exception:
     file = load_from_json_file("add_item.json")
+except Exception:
+    file = []
 
 file_name = "add_item.json"
 for argc in range(1, len(sys.argv)):
