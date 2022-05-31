5796dd47e67e7f289bda9c7721785840ca10c30f
Dak4rPrO <shinoflow111@gmail.com>
2022-05-31 14:05:22 -0700

    si

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" function that returns the dictionary description with 
+""" function that returns the dictionary description with
     simple data structure (list, dictionary, string,
     integer and boolean) for JSON serialization of an object """
 
