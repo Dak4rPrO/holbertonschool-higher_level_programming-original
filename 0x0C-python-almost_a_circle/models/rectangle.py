2f2a1a13e04ff5c030e7e72a7915f7ef078304f2
Dak4rPrO <4382@holbertonschool.com>
2022-06-05 17:17:02 -0300

    ESTABA MAL IDENTADO, SOY UN PETEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,7 +26,7 @@ class Rectangle(Base):
         """ def height """
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
@@ -40,7 +40,7 @@ class Rectangle(Base):
         """ def width """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
     
@@ -54,7 +54,7 @@ class Rectangle(Base):
         """ def x """
         if type(value) is not int:
             raise TypeError("x must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
@@ -68,7 +68,7 @@ class Rectangle(Base):
         """ def y """
         if type(value) != int:
             raise TypeError("y must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
         
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,17 +13,17 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ def init """
         super().__init__(size, size, x, y, id)
-        
-        def __str__(self):
-            """ def str """
-            return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}")
-        
-        @property
-        def size(self):
-            return self.height
-        
-        @size.setter
-        def size(self, value):
-            """ def size"""
-            self.width = value
-            self.height = value       
+
+    @property
+    def size(self):
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        """ def size"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """ def str """
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}")
