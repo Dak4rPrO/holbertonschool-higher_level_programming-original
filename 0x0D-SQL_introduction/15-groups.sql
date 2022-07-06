ea123ae65884d5ca29fd67d37fa8e739271b1887
Dak4rPrO <4382@holbertonschool.com>
2022-07-05 22:44:29 -0300

    15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server
-SELECT AVG(score)AS average FROM second_table
+SELECT COUNT(*) AVG(score)AS score FROM second_table GROUP BY number;
