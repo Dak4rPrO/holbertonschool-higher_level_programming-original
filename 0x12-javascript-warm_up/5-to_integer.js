ee336d44ba339e9236f04c2b0737915c84b919f3
Dak4rPrO <shinoflow111@gmail.com>
2022-08-01 17:26:25 -0300

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 
 const args = process.argv;
 
-if (args.length === 3) {
+if (args.length === 3 && parseInt(args[2])) {
   console.log('My number: ' + parseInt(args[2]));
-} else if (args.length < 3) {
+} else {
   console.log('Not a number ');
 }
