bb2711fa861774fe0f71d8792491790272fd7fdb
Dak4rPrO <shinoflow111@gmail.com>
2022-08-01 16:07:01 -0300

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,12 +2,10 @@
 
 const args = process.argv;
 
-if (args.length < 3){
-    console.log("No argument");
-}
-else if (args.length === 3){
-    console.log("Argument found");
-}
-else {
-    console.log("Arguments found");
+if (args.length < 3) {
+  console.log('No argument');
+} else if (args.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
