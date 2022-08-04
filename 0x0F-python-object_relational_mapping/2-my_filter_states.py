2c018c68c92c00d82fff1d7397d3903d8f8f6a9f
Dak4rPrO <4382@holbertonschool.com>
2022-08-04 14:21:02 -0700

    todo

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,13 +4,15 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    
+
+    arg = sys.argv[4]
+
     db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    
-    cur.execute("SELECT * FROM states")
+
+    cur.execute("SELECT * FROM states WHERE name = '%s'" % (arg))
     states = cur.fetchall()
-    
+
     for state in states:
         print(state)
 
