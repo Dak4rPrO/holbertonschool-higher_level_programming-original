0f8d5022d18031bea502471dd17648eeebee54d9
Dak4rPrO <shinoflow111@gmail.com>
2022-08-06 19:42:29 -0300

    todo pycode

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,11 +5,13 @@ import sys
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
 
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM states, \
-cities WHERE cities.state_id = states.id ORDER BY cities.id ASC""")
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM states,
+                cities WHERE cities.state_id = states.id
+                ORDER BY cities.id ASC""")
     cities = cur.fetchall()
 
     for state in cities:
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,13 +7,16 @@ if __name__ == "__main__":
 
     arg = sys.argv[4]
 
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
 
-    cur.execute("""SELECT cities.name, states.name FROM states, cities WHERE cities.state_id = states.id AND states.name = %s""", (arg,))
+    cur.execute("""SELECT cities.name, states.name FROM states, cities
+                WHERE cities.state_id = states.id
+                AND states.name = %s""", (arg,))
     cities = cur.fetchall()
 
-    for row in range(len(cities) -1):
+    for row in range(len(cities) - 1):
         print(cities[row][0],  end=', ')
         print(cities[row + 1][0])
 
