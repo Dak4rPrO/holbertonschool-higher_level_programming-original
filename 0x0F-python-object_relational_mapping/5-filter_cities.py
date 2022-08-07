8f2ad9daaf0c25da8153476704fa2eae373bb487
Dak4rPrO <shinoflow111@gmail.com>
2022-08-06 21:46:06 -0300

    5 a ver

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,17 +13,17 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
 
-    cur.execute("""SELECT cities.name, states.name FROM states, cities
+    cur.execute("""SELECT cities.name FROM states, cities
                 WHERE cities.state_id = states.id
                 AND states.name = %s""", (arg,))
     cities = cur.fetchall()
 
-    if (arg == False):
+    try:
         for row in range(len(cities) - 1):
-            print(cities[row][0],  end=', ')
+            print(cities[row][0], end=', ')
         print(cities[row + 1][0])
-    else:
-        print(" ")        
+    except Exception:
+        print()
 
     cur.close()
     db.close()
