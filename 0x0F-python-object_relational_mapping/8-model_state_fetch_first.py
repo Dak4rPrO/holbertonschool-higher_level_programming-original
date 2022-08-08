24ded8a53bec13bb1b9f3d004cf57172bd693b45
Dak4rPrO <shinoflow111@gmail.com>
2022-08-07 21:36:30 -0300

    8 definitivo

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" script that lists all State objects from the database hbtn_0e_6_usa """
+
+import MySQLdb
+import sys
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    class State(Base):
+        """Instatiation of class"""
+
+        __tablename__ = 'states'
+
+        id = Column(Integer, primary_key=True, unique=True,
+                    autoincrement=True, nullable=False)
+
+        name = Column(String(128), nullable=False)
+
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        result = session.query(State).all()
+
+        for r in result:
+            if (r is not None):
+                print(r.id, ': ', r.name, sep="")
+                break
+            else:
+                print("Nothing")
