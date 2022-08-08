2f075d6a3d3417c7fb273adcc59e054e5640d7c1
Dak4rPrO <shinoflow111@gmail.com>
2022-08-07 21:44:11 -0300

    8 definitivo??

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" script that lists all State objects from the database hbtn_0e_6_usa """
+"""script that lists all State objects that contain the letter a
+   from the database hbtn_0e_6_usa """
 
 import MySQLdb
 import sys
@@ -39,4 +40,4 @@ if __name__ == "__main__":
                 print(r.id, ': ', r.name, sep="")
                 break
             else:
-                print("Nothing")
+                print("Nothing\n")
