3c14f5e1a57e8c241865d9e8ef3137a9e0efa2c2
Dak4rPrO <4382@holbertonschool.com>
2022-08-19 17:54:28 -0300

    3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,9 +4,10 @@
     the email as a parameter, and displays
     the body of the response"""
 
-try:
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        html = response.read()
-        print(html.decode("utf-8"))
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            html = response.read()
+            print(html.decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
