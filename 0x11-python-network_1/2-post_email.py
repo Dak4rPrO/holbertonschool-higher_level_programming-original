b7ae4c9994c19b8c0e50a8ba6f0f6e2cd91f96e4
Dak4rPrO <4382@holbertonschool.com>
2022-08-19 20:45:09 +0000

    a

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,8 +4,9 @@ URL with the email as a parameter, and displays the body of the response """
 if __name__ == "__main__":
     from urllib import request, parse
     import sys
-    values = {'email': sys.argv[2]}  # email is the second argument
-    data = parse.urlencode(values).encode('ascii')  # encode data to ascii
-    req = request.Request(sys.argv[1], data)  # create request object
-    with request.urlopen(req) as response:  # open url and read response
+    values = {'email': sys.argv[2]}
+    data = parse.urlencode(values).encode('ascii')
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
+
