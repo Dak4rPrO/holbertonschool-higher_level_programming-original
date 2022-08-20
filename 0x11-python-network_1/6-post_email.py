31071485a12b964e28caa00f47d9098db06b66cf
Mauricio Miranda <98028958+Dak4rPrO@users.noreply.github.com>
2022-08-19 23:48:17 -0300

    probar checks 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Script that takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    
+    arg1 = sys.argv[1]
+    arg2 = sys.argv[2]
+    email = {'email': arg2}
+    response = requests.post(arg1, email)
+    print(response.text)
