be69ff33599d043ccd5bc77ca5ab5de0880cbc06
Dak4rPrO <4382@holbertonschool.com>
2022-08-22 15:12:45 -0300

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,12 +2,12 @@
 """ Script that takes in a URL, sends a request to the URL and
 displays the body of the response """
 
-import requests
-import sys
 
 if __name__ == "__main__":
-    r = requests.get(sys.argv[1])
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+    import requests
+    import sys
+    x = requests.get(sys.argv[1])
+    if x.status_code >= 400:
+        print("Error code: {}".format(x.status_code))
     else:
-        print(r.text)
+        print(x.text)
