88ecb96d36baab266667e7b399fef918d6b35da5
Dak4rPrO <4382@holbertonschool.com>
2022-08-22 15:15:50 -0300

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,14 +2,12 @@
 """Write a Python script that takes in a URL, sends a
 request to the URL and displays the body of the response."""
 
-import requests
-import sys
 
 if __name__ == "__main__":
-    
+    import requests
+    import sys
     user = sys.argv[1]
     passwrd = sys.argv[2]
-    
     url = 'https://api.github.com/user'
     req = requests.get(url, auth=(user, passwrd))
     print("{}".format(req.json().get('id')))
