602aa8bff3d0e7d1a3eff36108f37eadb1dd9554
Dak4rPrO <4382@holbertonschool.com>
2022-08-22 15:13:57 -0300

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,10 +2,10 @@
 """Write a Python script that takes in a URL, sends a
 request to the URL and displays the body of the response."""
 
-import requests
-import sys
 
 if __name__ == "__main__":
+    import requests
+    import sys
 
     if len(sys.argv) == 1:
         q = ""
