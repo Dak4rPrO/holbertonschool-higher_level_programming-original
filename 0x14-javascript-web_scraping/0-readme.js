eaba2235f19b759d8b5d255e8473bd51293d787a
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-09-12 15:45:46 -0300

    apt-get install dos2unix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-const argv = process.argv;
+
 const fs = require('fs');
-fs.readFile(argv[2], 'utf8', (err, data) => {
+const arg = process.argv;
+
+fs.readFile(arg[2], 'utf8', (err, data) => {
   if (err) {
     console.error(err);
-    return;
+    return
   }
   console.log(data);
 });
