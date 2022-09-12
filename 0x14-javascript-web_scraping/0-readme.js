647cde0f947f4548a4e7e21fc728e1e4b962f1bb
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-09-12 15:24:18 -0300

    probar 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const arg = process.argv;
 
 fs.readFile(arg[2], 'utf8', (err, data) => {
-    if (err) {
-        console.log(err)
-    }
-    console.log(data);
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
 });
