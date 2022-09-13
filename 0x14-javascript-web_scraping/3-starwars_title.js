2835912341dd46dae34605b90bae6c89a486f86f
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-09-12 21:02:17 -0300

    task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function starwars () {
+  const episode = process.argv[2];
+  const axios = require('axios');
+  const url = `https://swapi-api.hbtn.io/api/films/${episode}`;
+
+  axios.get(url)
+    .then((response) => {
+      console.log(`${response.data.title}`);
+    })
+
+    .catch((error) => {
+      console.log(`${error}`);
+    });
+}
+starwars();
